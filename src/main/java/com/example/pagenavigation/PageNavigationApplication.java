894175e11cf1e3d225ad8a1fd33cb0699685399d
JTRKor <47008368+JTRKor@users.noreply.github.com>
2026-03-26 19:40:06 -0700

    Create PageNavigationApplication.java

---
+++ src/main/java/com/example/pagenavigation/PageNavigationApplication.java
@@ -0,0 +1,11 @@
+package com.example.pagenavigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageNavigationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PageNavigationApplication.class, args);
+    }
+}
